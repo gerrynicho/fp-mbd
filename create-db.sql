41b87f24ac7d6f30f9098a91c639babc3d103256
Nicholas <5025231031@student.its.ac.id>
2025-06-10 12:58:40 +0700

    Update create-db.sql

--- create-db.sql
+++ create-db.sql
@@ -72,7 +72,7 @@ CREATE TABLE JADWAL_TAYANG (
     jadwal DATETIME NOT NULL,
     film_id_film CHAR(5) NOT NULL,
     teater_id_teater CHAR(5) NOT NULL,
-    FOREIGN KEY (film_id_film) REFERENCES FILM(id_film)
+    FOREIGN KEY (film_id_film) REFERENCES FILM(id_film),
     FOREIGN KEY (teater_id_teater) REFERENCES TEATER(id_teater)
 );
 
