595c5c9ffda1794bf4a6ae1eb212efd69abeba2f
KarsssV <163622473+KarsssV@users.noreply.github.com>
2025-06-16 20:51:14 +0700

    Update stored_procedure.sql

--- stored_procedure.sql
+++ stored_procedure.sql
@@ -85,48 +85,97 @@ DELIMITER ;
 
 -- #6 lokasi studio sedang tayang film apa saja
 DELIMITER $$
-CREATE PROCEDURE lokasi_studio_film_tayang()
+
+CREATE PROCEDURE get_film_by_merk_studio(IN merk_input VARCHAR(30))
 BEGIN
-    SELECT ls.id_lokasi_studio, ls.merk_studio, f.id_film, f.judul_film, f.genre, f.rating_usia
-    FROM LOKASI_STUDIO ls
-    JOIN TEATER t ON ls.id_lokasi_studio = t.lokasi_studio_id_lokasi_studio
-    JOIN JADWAL_TAYANG jt ON t.id_teater = jt.teater_id_teater
-    JOIN FILM f ON jt.film_id_film = f.id_film;
-END $$
+    SELECT DISTINCT 
+        F.id_film,
+        F.judul_film,
+        F.genre,
+        F.durasi,
+        F.sutradara,
+        F.rating_usia,
+        F.rating_film,
+        F.sinopsis
+    FROM LOKASI_STUDIO LS
+    JOIN TEATER T ON LS.id_lokasi_studio = T.lokasi_studio_id_lokasi_studio
+    JOIN JADWAL_TAYANG JT ON T.id_teater = JT.teater_id_teater
+    JOIN FILM F ON JT.film_id_film = F.id_film
+    WHERE LS.merk_studio = merk_input;
+END$$
+
 DELIMITER ;
 
 -- #7 teater dari lokasi studio yang menayang film
 DELIMITER $$
-CREATE PROCEDURE teater_lokasi_studio_film()
-BEGIN
-    SELECT ls.id_lokasi_studio, ls.nama_studio, s.id_studio, s.nama_teater, f.id_film, f.judul
-    FROM LOKASI_STUDIO ls
-    JOIN STUDIO s ON ls.id_lokasi_studio = s.lokasi_studio_id_lokasi_studio
-    JOIN JADWAL_TAYANG jt ON s.id_studio = jt.studio_id_studio
-    JOIN FILM f ON jt.film_id_film = f.id_film;
-END $$
 
--- #8 jadwal tayang di studio tertentu
-DELIMITER $$
-CREATE PROCEDURE jadwal_tayang_studio_tertentu(p_studio_id CHAR(5))
+CREATE PROCEDURE teater_tempat_film_ditayangkan(
+    IN id_film CHAR(5),
+    IN id_lokasi_studio CHAR(5)
+)
 BEGIN
-    SELECT jt.id_jadwal, f.judul, jt.tanggal_tayang, jt.waktu_tayang
+    SELECT DISTINCT 
+        T.id_teater,
+        T.jumlah_kursi_tersedia,
+        L.alamat_studio,
+        L.merk_studio
+    FROM JADWAL_TAYANG JT
+    JOIN TEATER T ON JT.teater_id_teater = T.id_teater
+    JOIN LOKASI_STUDIO L ON T.lokasi_studio_id_lokasi_studio = L.id_lokasi_studio
+    WHERE JT.film_id_film = id_film
+      AND T.lokasi_studio_id_lokasi_studio = id_lokasi_studio;
+END$$
+
+DELIMITER ;
+
+-- #8 jadwal tayang di lokasi tertentu
+DELIMITER $$
+
+CREATE PROCEDURE jadwal_tayang_film_lokasi(
+    IN p_judul_film VARCHAR(50),
+    IN p_id_lokasi CHAR(5)
+)
+BEGIN
+    SELECT 
+        jt.id_tayang,
+        f.judul_film,
+        jt.jadwal,
+        ls.alamat_studio,
+        t.id_teater
     FROM JADWAL_TAYANG jt
     JOIN FILM f ON jt.film_id_film = f.id_film
-    WHERE jt.studio_id_studio = p_studio_id;
-END $$
+    JOIN TEATER t ON jt.teater_id_teater = t.id_teater
+    JOIN LOKASI_STUDIO ls ON t.lokasi_studio_id_lokasi_studio = ls.id_lokasi_studio
+    WHERE f.judul_film = p_judul_film
+      AND ls.id_lokasi_studio = p_id_lokasi;
+END$$
+
 DELIMITER ;
 
 -- #9 film tersedia berdasarkan tanggal dan lokasi studio
 DELIMITER $$
-CREATE PROCEDURE film_tersedia_tanggal_lokasi(p_tanggal DATE, p_lokasi_studio_id CHAR(5))
+
+CREATE PROCEDURE film_tersedia_tanggal_lokasi(
+    IN p_tanggal DATE,
+    IN p_id_lokasi CHAR(5)
+)
 BEGIN
-    SELECT f.id_film, f.judul, jt.tanggal_tayang, jt.waktu_tayang
-    FROM FILM f
-    JOIN JADWAL_TAYANG jt ON f.id_film = jt.film_id_film
-    JOIN STUDIO s ON jt.studio_id_studio = s.id_studio
-    WHERE jt.tanggal_tayang = p_tanggal AND s.lokasi_studio_id_lokasi_studio = p_lokasi_studio_id;
-END $$
+    SELECT DISTINCT
+        f.id_film,
+        f.judul_film,
+        f.genre,
+        f.durasi,
+        f.sutradara,
+        f.rating_usia,
+        f.rating_film
+    FROM JADWAL_TAYANG jt
+    JOIN FILM f ON jt.film_id_film = f.id_film
+    JOIN TEATER t ON jt.teater_id_teater = t.id_teater
+    JOIN LOKASI_STUDIO ls ON t.lokasi_studio_id_lokasi_studio = ls.id_lokasi_studio
+    WHERE DATE(jt.jadwal) = p_tanggal
+      AND ls.id_lokasi_studio = p_id_lokasi;
+END$$
+
 DELIMITER ;
 
 -- #10 pembatalan transaksi
